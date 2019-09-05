9e2494b83c88af227219ee3191d7abf59d41950e
Ilya <iliya.yurkevic@mail.ru>
2019-09-05 21:59:09 +0300

    Добавьте файлы проекта.

---
+++ SquareRoot.cpp
@@ -0,0 +1,35 @@
+﻿#include "iostream"
+#include "vector"
+using namespace std;
+
+void Root(double a)
+{
+	double epicilon = 0.00001;
+	double low = 0;
+	double high = a;
+	double mid = (high + low) / 2;
+
+	while (abs(pow(mid, 2) - a) >= epicilon)
+	{
+		if (pow(mid, 2) < a)
+		{
+			low = mid;
+		}
+		else
+		{
+			high = mid;
+		}
+		mid = (high + low) / 2;
+	}
+	cout << "Квадратный корень числа " << a << " равен " << mid << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int X;
+	cout << "Введите число, квадратный корень которого необходимо вычислить:\n";
+	cin >> X;
+	Root(X);
+	return 0;
+}
